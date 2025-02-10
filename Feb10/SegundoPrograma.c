4406d8d3a91a7c3e73355dcc5047fe54c0f4967f
Uriel12-03 <ue.zunigamartinez@ugto.mx>
2025-02-10 11:51:05 -0600

    El segundo programa Uriel Zuniga

---
+++ Feb10/SegundoPrograma.c
@@ -0,0 +1,10 @@
+/*Creado el 10 de Febrero del 2025 por Uriel ZUniga*/
+
+#include<stdio.h>
+
+int main ( ) 
+{
+       printf("Este es mi segundo programa, Hala Madrid\n");
+       printf("Gracias, hasta pronto\n");
+}
+ /*Termine el segundo programa*/
