c12fb11a1b337a5007938a0d954e3f7f8b95125c
Uriel12-03 <ue.zunigamartinez@ugto.mx>
2025-02-10 11:27:03 -0600

    Primer Programa 10Feb Uriel Zuniga

---
+++ Feb10/PrimerPrograma.c
@@ -0,0 +1,11 @@
+/* Creado el 10 de Febrero del 2025 por Uriel Zuniga */
+
+#include<stdio.h>
+
+int main ( )
+{
+     printf("Hola Uriel \n");
+     printf("Este es mi primer programa del curso\n");
+}
+
+/*Termine de escribir el programa*/ 
