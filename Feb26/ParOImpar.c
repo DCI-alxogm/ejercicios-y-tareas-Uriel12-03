6ae2dd3c10b08b2c7e715567a9da3a737037873c
Uriel12-03 <ue.zunigamartinez@ugto.mx>
2025-02-26 12:48:06 -0600

    Par o impar y Vocal o Consonante

--- Feb26/ParOImpar.c
+++ Feb26/ParOImpar.c
@@ -1,31 +1,20 @@
-/* Programa Par o Impar Uriel Zuniga Feb26 */
+/* Programa Vocal o Consonante Uriel Zuniga Feb26 */
 
 #include <stdio.h>
-#include <stdlib.h>
 
 int main ( ) {
 
-        char respuesta[2];
-        int num, res;
-
-        printf("Cual numero quieres saber si es par o impar\n");
-        scanf("%i", &num);
-        res=num%2;
-        if(res==0){
-           printf("%i es par \n", num);
-           } else {
-                  printf("%i es impar \n", num);
-                      }
-
-      printf("Deseas agregar otro valor? \n");
-      scanf("%s", respuesta);
-      if(respuesta == Si){
-         printf("%s Aun no se como hacer eso \n", SI);
-         } else {
-                  printf("%s Bueno, adios \n", No);  
-
-       
-        return(0);
-
+     char c;
+     int es_vocalminuscula, es_vocalmayuscula;
+     printf("Ingresa una letra: ");
+     scanf("%c", %c);
+    
+     es_vocalminuscula = (c == "a" || c == "e" || c == "i" || c == "o" || c == "u");
+     es_vocalmayuscula = (c == "A" || c == "E" || c == "I" || c == "O" || c == "U");
+     If (es_vocalmayuscula || es_vocalminuscula) {
+          printf("%c es una vocal\n", c);
+          } else {
+                 printf("%c es consonante \n", c);
+        }
+return(0); 
 }
-        
