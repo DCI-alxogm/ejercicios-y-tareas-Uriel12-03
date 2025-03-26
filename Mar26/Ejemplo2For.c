0005a425eeae8167dd4231184697ee53af5cf8f1
Uriel12-03 <ue.zunigamartinez@ugto.mx>
2025-03-26 11:44:09 -0600

    Ejemplo 2 For Uriel Zuniga

---
+++ Mar26/Ejemplo2For.c
@@ -0,0 +1,17 @@
+/* Ejemplo 2 For Uriel Zuniga Marzo 26 */
+
+#include<stdio.h>
+#include<math.h>
+
+int main ()
+{
+         float exp_;
+         int n=10, j;
+
+         for(j=0; j<n; j++){
+              exp_=exp(j);
+              printf("%i \t %f\n", j, exp_);
+         }
+return 0;
+
+}
